431149328c9379d92472b14ab6d1e8817f1128e6
Erik Tollerud <erik.tollerud@gmail.com>
2016-01-31 04:42:21 -0500

    added simple plot function

--- bme280.py
+++ bme280.py
@@ -413,3 +413,27 @@ class BME280Recorder:
             timeleft = sttime - time.time() + 30
             if timeleft > 0:
                 time.sleep(timeleft)
+
+def triple_plots(fntab):
+    from matplotlib import pyplot as plt
+    from astropy.table import Table
+    from astropy.time import Time
+
+    if isinstance(fntab, str):
+        tab = table.Table.read('firstout.cal',format='csv')
+
+    ts=Time([time.mktime(time.strptime(ti,'%Y-%m-%d_%H:%M:%S'))-5*3600. for ti in tab['time']],format='unix').plot_date
+
+    ax1 = plt.subplot(3,1,1)
+    ax2 = plt.subplot(3,1,2, sharex=ax1)
+    ax3 = plt.subplot(3,1,3, sharex=ax1)
+
+    ax1.plot_date(ts, tab['temperature'],'-')
+    ax1.set_ylabel('Temperature')
+    ax2.plot_date(ts, tab['pressure'],'-')
+    ax2.set_ylabel('Pressure')
+    ax3.plot_date(ts, tab['humidity'],'-')
+    ax3.set_ylabel('Humidity')
+
+    plt.subplots_adjust(hspace=0)
+    plt.tight_layout()
