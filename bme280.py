b80afe50c8932feaf946f5b3f3a67c0417dd8715
Erik Tollerud <erik.tollerud@gmail.com>
2016-01-31 17:54:25 -0500

    minor fixes

--- bme280.py
+++ bme280.py
@@ -420,13 +420,13 @@ def triple_plots(fntab):
     from astropy.time import Time
 
     if isinstance(fntab, str):
-        tab = table.Table.read('firstout.cal',format='csv')
+        tab = Table.read(fntab, format='csv')
 
-    ts=Time([time.mktime(time.strptime(ti,'%Y-%m-%d_%H:%M:%S'))-5*3600. for ti in tab['time']],format='unix').plot_date
+    ts = Time([time.mktime(time.strptime(ti,'%Y-%m-%d_%H:%M:%S')) - 5*3600. for ti in tab['time']],format='unix').plot_date
 
-    ax1 = plt.subplot(3,1,1)
-    ax2 = plt.subplot(3,1,2, sharex=ax1)
-    ax3 = plt.subplot(3,1,3, sharex=ax1)
+    ax1 = plt.subplot(3, 1, 1)
+    ax2 = plt.subplot(3, 1, 2, sharex=ax1)
+    ax3 = plt.subplot(3, 1, 3, sharex=ax1)
 
     ax1.plot_date(ts, tab['temperature'],'-')
     ax1.set_ylabel('Temperature')
@@ -435,5 +435,5 @@ def triple_plots(fntab):
     ax3.plot_date(ts, tab['humidity'],'-')
     ax3.set_ylabel('Humidity')
 
-    plt.subplots_adjust(hspace=0)
     plt.tight_layout()
+    plt.subplots_adjust(hspace=0)
