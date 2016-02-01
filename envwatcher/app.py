ca5d811f7e0397a18bd52ba07592331c09fd5335
Erik Tollerud <erik.tollerud@gmail.com>
2016-01-31 19:12:54 -0500

    framework of app

--- envwatcher/app.py
+++ envwatcher/app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__.split('.')[0])
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/index")
+def index():
+    return render_template('index.html', series=series)
+
+
+@app.route("/plots/<plotname>")
+def index(plotname):
+    return render_template('generating.html', series_name=series_name, plots=plots)
+    return render_template('plots.html', series_name=series_name, plots=plots)
 
 if __name__ == "__main__":
     app.run()
