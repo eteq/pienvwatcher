e68357966ca16809bbc8ceae99798f0e6f33e35e
Erik Tollerud <erik.tollerud@gmail.com>
2016-02-03 02:34:51 -0500

    ordering fix

--- envwatcher/webapp.py
+++ envwatcher/webapp.py
@@ -63,14 +63,15 @@ def index():
 @app.route("/series/<series_name>")
 def series(series_name):
     dsetdir = os.path.join(app.root_path, app.config['DATASETS_DIR'])
+    progressfn = os.path.join(dsetdir, app.config['PROGRESS_NAME'])
     infodct = {}
     recorder_present = check_for_recorder(progressfn, infodct)
+
     if app.config['ALWAYS_MAKE_PLOTS_AT_SERIES_REQUEST'] or not recorder_present:
         dsetfn = os.path.join(dsetdir, series_name + '_cal')
         plotsdir = os.path.join(app.root_path, app.config['PLOTS_DIR'])
         plot_names = write_series_plots(dsetfn, plotsdir, app.config['DEG_F'])
     else:
-        progressfn = os.path.join(dsetdir, app.config['PROGRESS_NAME'])
         plot_names = [pair.split(',') for pair in infodct['Plot names'].split(';')]
 
     plots = [dict(name=nm, path='/plots/{}?{}'.format(path,time.time()))
